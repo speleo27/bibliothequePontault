5e653b3b33d107f109276bceedee58cc683a64c6
speleo27 <seb10400@orange.fr>
2021-09-16 07:51:06 +0200

    prepare project first commit

---
+++ src/main/java/blibliothequepontault/bibliothequepontault/BibliothequePontaultApplication.java
@@ -0,0 +1,13 @@
+package blibliothequepontault.bibliothequepontault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliothequePontaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliothequePontaultApplication.class, args);
+    }
+
+}
