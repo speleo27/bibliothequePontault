ce046d27f72380b7c163411fdbe1b0d1cb763784
speleo27 <seb10400@orange.fr>
2021-09-18 17:13:36 +0200

    fix(deploy) ref #2 work on helloworld to configure view before start and test deploy

--- src/main/java/blibliothequepontault/bibliothequepontault/BibliothequePontaultApplication.java
+++ src/main/java/blibliothequepontault/bibliothequepontault/BibliothequePontaultApplication.java
@@ -1,13 +1,47 @@
 package blibliothequepontault.bibliothequepontault;
 
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import javax.annotation.security.RolesAllowed;
+import java.util.Arrays;
+import java.util.Locale;
+@Controller
 @SpringBootApplication
-public class BibliothequePontaultApplication {
+public class BibliothequePontaultApplication implements ApplicationRunner, CommandLineRunner, WebMvcConfigurer {
 
+    @RolesAllowed("*")
     public static void main(String[] args) {
         SpringApplication.run(BibliothequePontaultApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception{
+        System.out.println("hello runner works" );
+    }
+    @Override
+    public void run(String... args) throws Exception{
+        System.out.println("hello runner cmd");
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.FRANCE);
+        return slr;
+    }
+    @GetMapping(value="/helloWorld")
+    public String getHelloWorldVue(){
+        return "helloWorld";
+    }
 }
